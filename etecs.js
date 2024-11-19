c3d75b4c40ee0882c75ae18a102635871b36b3bb
Gustavo-dev <163581883+Gustavo-dev-tech@users.noreply.github.com>
2024-11-19 00:17:56 -0300

    Add files via upload
    Signed-off-by: Gustavo-dev <163581883+Gustavo-dev-tech@users.noreply.github.com>

--- etecs.js
+++ etecs.js
@@ -9,3 +9,15 @@ fetch('etecs.json') // Supondo que você tenha um arquivo chamado etecs.json
         });
     })
     .catch(error => console.error('Erro ao carregar os dados das ETECs:', error));
+
+const $list = document.querySelectorAll('li');
+function activeLink() {
+    $list.forEach(($li) => {
+        $li.classList.remove('active');
+    });
+    this.classList.add('active');
+}
+
+$list.forEach(($li) => {
+    $li.addEventListener('click', activeLink);
+});
